2c7226bb9d2cfa3cdd7ca46c11fc28e848d238c1
yndvp <58580623+yndvp@users.noreply.github.com>
2021-06-21 15:25:48 +0900

    fix slider navigator error

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const navigator2 = document.querySelector(".navigation-btn.btn2");
 const navigator3 = document.querySelector(".navigation-btn.btn3");
 const firstSlide = document.querySelector(".slide1");
 
+// Remove active class
+const removeActive = function () {
+  navigators.forEach(function (el) {
+    el.classList.remove("active");
+  });
+};
+
 // Slider manual player
 navigator1.addEventListener("click", function () {
   firstSlide.style.marginTop = "0";
@@ -20,19 +27,23 @@ navigator3.addEventListener("click", function () {
 setInterval(function () {
   if (firstSlide.style.marginTop === "0px") {
     firstSlide.style.marginTop = "-630px";
+    removeActive();
+    navigators[1].classList.add("active");
   } else if (firstSlide.style.marginTop === "-630px") {
     firstSlide.style.marginTop = "-1260px";
+    removeActive();
+    navigators[2].classList.add("active");
   } else {
     firstSlide.style.marginTop = "0px";
+    removeActive();
+    navigators[0].classList.add("active");
   }
 }, 5000);
 
 // Activate slider navigators
 navigators.forEach(function (navigator) {
   navigator.addEventListener("click", function () {
-    navigators.forEach(function (el) {
-      el.classList.remove("active");
-    });
+    removeActive();
     navigator.classList.add("active");
   });
 });
