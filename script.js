cabae4e1f0ea197b9edb05e324cc7510ac4610f4
yndvp <58580623+yndvp@users.noreply.github.com>
2021-06-24 23:39:40 +0900

    add javascript to testimonials

--- script.js
+++ script.js
@@ -1,49 +1,76 @@
-// Home slider variables
-const navigators = document.querySelectorAll(".navigation-btn");
-const navigator1 = document.querySelector(".navigation-btn.btn1");
-const navigator2 = document.querySelector(".navigation-btn.btn2");
-const navigator3 = document.querySelector(".navigation-btn.btn3");
-const firstSlide = document.querySelector(".slide1");
+// Slider variables
+const sliderNavs = document.querySelectorAll(".slider .navigation-btn");
+const sliderNav1 = document.querySelector(".slider .navigation-btn.btn1");
+const sliderNav2 = document.querySelector(".slider .navigation-btn.btn2");
+const sliderNav3 = document.querySelector(".slider .navigation-btn.btn3");
+const sliderFirst = document.querySelector(".slider .slide1");
 
 // Remove active class
 const removeActive = function () {
-  navigators.forEach(function (el) {
+  sliderNavs.forEach(function (el) {
     el.classList.remove("active");
   });
 };
 
 // Slider manual player
-navigator1.addEventListener("click", function () {
-  firstSlide.style.marginTop = "0";
+sliderNav1.addEventListener("click", function () {
+  sliderFirst.style.marginTop = "0";
 });
-navigator2.addEventListener("click", function () {
-  firstSlide.style.marginTop = "-630px";
+sliderNav2.addEventListener("click", function () {
+  sliderFirst.style.marginTop = "-630px";
 });
-navigator3.addEventListener("click", function () {
-  firstSlide.style.marginTop = "-1260px";
+sliderNav3.addEventListener("click", function () {
+  sliderFirst.style.marginTop = "-1260px";
 });
 
 // Slider auto player
 setInterval(function () {
-  if (firstSlide.style.marginTop === "0px") {
-    firstSlide.style.marginTop = "-630px";
+  if (sliderFirst.style.marginTop === "0px") {
+    sliderFirst.style.marginTop = "-630px";
     removeActive();
-    navigators[1].classList.add("active");
-  } else if (firstSlide.style.marginTop === "-630px") {
-    firstSlide.style.marginTop = "-1260px";
+    sliderNavs[1].classList.add("active");
+  } else if (sliderFirst.style.marginTop === "-630px") {
+    sliderFirst.style.marginTop = "-1260px";
     removeActive();
-    navigators[2].classList.add("active");
+    sliderNavs[2].classList.add("active");
   } else {
-    firstSlide.style.marginTop = "0px";
+    sliderFirst.style.marginTop = "0px";
     removeActive();
-    navigators[0].classList.add("active");
+    sliderNavs[0].classList.add("active");
   }
 }, 5000);
 
 // Activate slider navigators
-navigators.forEach(function (navigator) {
+sliderNavs.forEach(function (navigator) {
   navigator.addEventListener("click", function () {
     removeActive();
     navigator.classList.add("active");
+  });
+});
+
+// Testimonials variables
+const testimonialNavs = document.querySelectorAll(
+  ".testimonials .navigation-btn"
+);
+const testimonialFirst = document.querySelector(".testimonials .first");
+
+// Testimonials manual player
+testimonialNavs[0].addEventListener("click", function () {
+  testimonialFirst.style.marginLeft = "0%";
+});
+testimonialNavs[1].addEventListener("click", function () {
+  testimonialFirst.style.marginLeft = "-33.33%";
+});
+testimonialNavs[2].addEventListener("click", function () {
+  testimonialFirst.style.marginLeft = "-66.66%";
+});
+
+// Activate testimonials navigators
+testimonialNavs.forEach(function (el) {
+  el.addEventListener("click", function () {
+    testimonialNavs.forEach(function (el2) {
+      el2.classList.remove("active");
+    });
+    el.classList.add("active");
   });
 });
