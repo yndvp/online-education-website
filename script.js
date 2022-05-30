da74df31f25c799352c5ad0a03f1ab37fe574844
yndvp <58580623+yndvp@users.noreply.github.com>
2022-05-30 08:12:59 -0400

    update date

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ menuLinksBox.addEventListener('click', function (e) {
 ///////////////////////////////////////
 // Day counter
 const countdown = function () {
-  const countDate = new Date('February 30, 2022 00:00:00').getTime();
+  const countDate = new Date('July 30, 2022 00:00:00').getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
